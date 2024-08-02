471bedb01fead95fb190f7a7a77b39ebcad76c75
sos-robin <sos.robinson044@gmail.com>
2024-08-02 18:54:28 +0300

    Initial commit

---
+++ Mzuri/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    
+    return render(request, 'Mzuri/index.html')
+
+def About(request):
+    
+    return render(request,'Mzuri/about.html')
+
+
+def Contact(request):
+    
+    return render(request,'Mzuri/contact.html')
+
+def Services(request):
+    
+    return render(request,'Mzuri/services.html')
+
+def Portfolio(request):
+    
+    return render(request,'Mzuri/work.html')
