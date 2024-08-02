3eb2035cec884ef6c3ce96d6388ce2fce94b8959
sos-robin <sos.robinson044@gmail.com>
2024-08-02 19:14:06 +0300

    Add requirements.txt

--- mzuri_spaces/settings.py
+++ mzuri_spaces/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#^gx3cp(+kb)%k*c3$%6peig5y!x+%$*$wp6iu@b@#apoa+uni
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
