96c1d60449b7fbd966db099191cd2412466a6d94
Robinson Sos <57221931+sos-robin@users.noreply.github.com>
2025-05-28 14:58:08 +0300

    changes

--- mzuri_spaces/settings.py
+++ mzuri_spaces/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'mzuri_spaces.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if 'vercel.app' in ALLOWED_HOSTS or 'www.mzurispaces.co.ke' in ALLOWED_HOSTS:
-    DATABASES = {
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'postgres',
@@ -89,13 +89,13 @@ if 'vercel.app' in ALLOWED_HOSTS or 'www.mzurispaces.co.ke' in ALLOWED_HOSTS:
             'PORT': '6543',
         }
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
